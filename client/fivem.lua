00996f896c3a7a9ffec8367fc3d0b3007e22f4dd
Tom Witkowski <dev.gummibeer@gmail.com>
2017-06-11 15:16:09 +0200

    add fivem client preview

---
+++ client/fivem.lua
@@ -0,0 +1,11 @@
+-- Sets the configuration for pop-up headlights for a vehicle model.
+-- @module fivem
+-- @link https://wiki.fivem.net/wiki/SetModelHeadlightConfiguration
+-- @usage SetModelHeadlightConfiguration('ruiner2', 120.0, 37.5, false)
+-- @usage SetModelHeadlightConfiguration('tropos', 30.0, 40.0, false)
+-- @param modelHash Model
+-- @param rotationPerSecond float
+-- @param rotation float
+-- @param invertRotation boolean
+-- @return void
+function SetModelHeadlightConfiguration(modelHash, rotationPerSecond, rotation, invertRotation) end
