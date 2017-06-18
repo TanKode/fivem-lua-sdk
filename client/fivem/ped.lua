80974651d0e1cb78158685a4f912df23ee03bd2a
Tom Witkowski <dev.gummibeer@gmail.com>
2017-06-18 19:41:45 +0200

    add client fivem ped

--- client/fivem/ped.lua
+++ client/fivem/ped.lua
@@ -6,3 +6,24 @@
 -- @param toggle bool
 -- @return void
 function SetPedIsDrunk(ped, toggle) end
+
+-- Get information about if a ped is shooting or not
+-- @module fivem
+-- @submodule ped
+-- @link https://wiki.fivem.net/wiki/IsPedShooting
+-- @usage pedShooting = IsPedShooting(GetPlayerPed(-1))
+-- @param ped Ped
+-- @return bool
+function IsPedShooting(ped) end
+
+-- Give a weapon with ammo to ped
+-- @module fivem
+-- @submodule ped
+-- @link https://wiki.fivem.net/wiki/GiveWeaponToPed
+-- @usage GiveWeaponToPed(GetPlayerPed(-1), GetHashKey("weapon_pistol"), 100, false)
+-- @param ped Ped
+-- @param weaponHash hash
+-- @param ammo int
+-- @param unknown bool
+-- @return void
+function GiveWeaponToPed(ped, weaponHash, ammo, unknown) end
